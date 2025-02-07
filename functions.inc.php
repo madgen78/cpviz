d5016b8cb5840a7390b98264d4f6de8b60deab3d
Adam Volchko <48374773+madgen78@users.noreply.github.com>
2025-02-07 12:38:35 -0800

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -597,8 +597,8 @@ function dp_follow_destinations (&$route, $destination) {
   //preg_match not found
   } elseif (preg_match("/^qmember(Ext(\d+).+)/", $destination, $matches)) {
 		$qlabel=$matches[1];
-		if (isset($u[$dest]['name']) && $u[$dest]['name']!=''){echo $u[$dest]['name'];}
 		$qextension=$matches[2];
+	  	if (isset($u[$qextension]) && $u[$qextension]['name']!=''){echo $u[$qextension]['name'];}
 		$node->attribute('label', $qlabel);
 		$node->attribute('URL', htmlentities('/admin/config.php?display=extensions&extdisplay='.$qextension));
 		$node->attribute('target', '_blank');
